3080d2d747ff466b45af8e4f764c0057ea14d4eb
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-25 11:10:25 -0700

    aost: Move QC board variables earlier
    These variables are usually set in a device's BoardConfig, setting them
    in qcom_target is at the very end of the "configuration process" which
    results in them being unavailable to plenty of other configuration
    "things" (ex. soong namespaces or soong config). Move them to right
    after a device's BoardConfig has been found and loaded.
    
    Change-Id: Iddd731202d22ed3f8eb010197ce20d3c75a1f40a

---
+++ config/BoardConfig.mk
@@ -0,0 +1,8 @@
+# Charger
+ifeq ($(WITH_LINEAGE_CHARGER),true)
+    BOARD_HAL_STATIC_LIBRARIES := libhealthd.lineage
+endif
+
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+include vendor/aost/config/BoardConfigQcom.mk
+endif
