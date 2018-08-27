6b0beb0e03f5a11aab4e815cb7d6a158e5e75471
Ahmed Hady <ahmedhady6@gmail.com>
2018-08-28 00:13:45 +0200

    Revert "ENFORCE RRO"
    This reverts commit 3380513e26aac603e1648de148b2baac72a20bcb.

--- config/common.mk
+++ config/common.mk
@@ -148,10 +148,8 @@ PRODUCT_COPY_FILES += \
     vendor/aost/prebuilts/lib64/libsketchology_native.so:system/lib64/libsketchology_native.so
 
 # Include package overlays
-BOARD_ENFORCE_RRO := true
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/aost/overlay
 DEVICE_PACKAGE_OVERLAYS += \
-    vendor/aost/overlay/
+    vendor/aost/overlay/common/
 
 # Compile Accents Overlay
 PRODUCT_PACKAGES += \
